daa7acb1030ec32de03752e2a9127e4c57eb17ba
Shriram Srinivasan <shri16ram@gmail.com>
2023-02-28 23:45:52 +0530

    small changes

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask,render_template
 import json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(length=30),nullable=False,unique=True)
+    price=db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12),nullable=False,unique=True)
+    description = db.Column(db.String(length=1024),nullable=False,unique=True)
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -21,3 +34,7 @@ def market_page():
 # @app.route('/about/<username>')
 # def about_page(username):
 #     return f'<h1>This is the about page for {username}</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
